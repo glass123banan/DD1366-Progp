d9cfc4c2b52f8d3a38b47599d48db88f74907cc3
glass123banan <glass123banan@gmail.com>
2025-01-31 01:18:36 +0100

    uppg2

---
+++ mästarprov1/sirSimulate.hs
@@ -0,0 +1,9 @@
+sirSimulate :: Double -> Double -> Int -> Int -> (Int, Int, Int) -> [(Int, Int, Int)]
+sirSimulate beta gamma n steps initialState = 
+
+
+
+
+main :: IO()
+main = do
+    -- test o sånt
