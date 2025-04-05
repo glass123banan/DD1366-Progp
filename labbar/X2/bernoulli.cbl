fa436a860b2d89415b50a1578c943372f8f72f9c
glass123banan <glass123banan@gmail.com>
2025-04-05 20:17:39 +0200

    smalltalk done

---
+++ labbar/X2/bernoulli.cbl
@@ -0,0 +1,9 @@
+        *>    RUN FILE:
+        *>             cobc -x bernoulli.cbl
+        *>             ./bernoulli
+    
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. bernoulli.
+       PROCEDURE DIVISION.
+           DISPLAY "Hello, COBOL!".
+           STOP RUN.
